929f19edc8351726d41a7795edc16606044d27f3
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-03-17 20:51:47 +0200

    added initial dynamism and 2 main parts(html)

---
+++ math_utilities.js
@@ -0,0 +1,20 @@
+//adding menu icon and its function
+$(document).ready(function() {
+    $("#menuicon").on("click", function() {
+          $(".menudiv").toggle("slow");
+          $("#menuicon").toggleClass("activeX");
+        if($("#menuicon").hasClass("activeX")){
+            $("#menuicon").fadeOut("slow", function() {
+                $(this).text("✖").fadeIn("slow");
+            });
+        }else{
+            $("#menuicon").fadeOut("slow", function() {
+                $(this).text("☰").fadeIn("slow");
+            });
+        }
+    });
+    $(".mathutils").on("click", function() {
+        $("#subcomponents").toggle();
+        $(".arrowhead").toggleClass("arrowtoggle");
+    })
+});
