4e63e34be139cd48159f89124fc99e4c8a9fdf8a
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-03-18 22:29:57 +0200

    added proportionality calc and second order EQ calc

--- math_utilities.js
+++ math_utilities.js
@@ -17,4 +17,40 @@ $(document).ready(function() {
         $("#subcomponents").toggle();
         $(".arrowhead").toggleClass("arrowtoggle");
     })
+    $(".functionstitle").on("click",function() {
+        $("#functiondiv").toggle();
+        $(".arrowhead_sec").toggleClass("arrowtoggle");
+    })
+
+    $("#calcprop").on("click",function(){
+        if($("#direct").prop("checked")){
+            const result=$("#yvalue").val()/$("#xvalue").val();
+            $("#kvalue").val(result);
+            $("#xvalue").val("");
+            $("#yvalue").val("");
+        }else{
+            const result=$("#yvalue").val()*$("#xvalue").val();
+            $("#kvalue").val(result);
+            $("#xvalue").val("");
+            $("#yvalue").val("");
+        }
+        
+    })
+
+    $("#calcsecorder").on("click",function(){
+        const avalue=Number($("#avalue").val());
+        const bvalue=Number($("#bvalue").val());
+        const cvalue=Number($("#cvalue").val());
+        const deltasq=Math.sqrt(bvalue**2-(4*avalue*cvalue));
+
+        if (isNaN(deltasq)){
+            const error="No real roots";
+            $("#errorsecorder").text(error);
+        }else{
+            const result1= ((-bvalue+deltasq)/(2*avalue)).toFixed(2);
+            const result2=((-bvalue-deltasq)/(2*avalue)).toFixed(2);
+            $("#resultvalue").val("x1="+result1 +" / "+"x2="+ result2);
+        }
+    })
 });
+
